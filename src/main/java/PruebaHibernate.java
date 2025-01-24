fa686204462a39a3420f7f5e93aedac1f901d925
elarreglador <elarreglador@protonmail.com>
2025-01-24 19:25:09 +0100

    ✨ Cambio menor: Consulta HSL SELECT FROM WHERE AND

--- src/main/java/PruebaHibernate.java
+++ src/main/java/PruebaHibernate.java
@@ -17,18 +17,19 @@ public class PruebaHibernate {
 		        
 		        // Muchas consultas requieren un @OverWrite de toString() del objeto
 		        // Consulta de empleados ( imprime objeto.toString() )
-		        System.out.println("Listado 1: Listado de Empleados");
+	        	System.out.println("\nConsulta de empleados");
 		        Query<Empleados> query = session.createQuery( "FROM Empleados", Empleados.class );
 		        List<Empleados> list = query.list();
 		        list.forEach(e-> System.out.println( e.toString() ) );
 		        
-		        // Consulta de empleados mas elegante ( imprime objeto.toString() )
-		        System.out.println("Listado 2: Listado de Empleados");
-		        session.createQuery("FROM Empleados", Empleados.class)
+		        // Consulta de departamentos mas elegante ( imprime objeto.toString() )
+		        System.out.println("\nConsulta de departamentos mas elegante");
+		        session.createQuery("FROM Departamentos", Departamentos.class)
 		        	.list()
 		        	.forEach(e-> System.out.println( e.toString() ) );
 		        
 		        // Empleados que cobren mas de un minimo ( imprime dos campos )
+		        System.out.println("\nEmpleados que cobren mas de un minimo");
 		        float minimo = 1000f;
 		        Query<Object[]> query3 = session.createQuery(
 		                "SELECT E.apellido, E.salario " +
@@ -37,6 +38,29 @@ public class PruebaHibernate {
 		        query3.setParameter("minimo", minimo);
 		        List<Object[]> resultados = query3.list();
 		        resultados.forEach(r -> System.out.println( r[0] + " | " + r[1]));
+		        
+		        // Empleados de departamento Ventas
+		        System.out.println("\nEmpleados de departamento Ventas");
+		        String dep = "ventas";
+		        Query<Object[]> query4 = session.createQuery(
+		                "SELECT E.empNo, E.apellido " +
+		                "FROM Empleados E " +
+		                "WHERE E.departamentos.dnombre = :dep", Object[].class);
+		        query4.setParameter("dep", dep);
+		        List<Object[]> resultados4 = query4.list();
+		        resultados4.forEach(r -> System.out.println("Emp_no: " + r[0] + ", " + r[1]));
+		        
+		        // Empleados de departamento Ventas con salario > 1500
+		        System.out.println("\nEmpleados de departamento Ventas con salario > 1500");
+		        String dep5 = "ventas";
+		        Query<Object[]> query5 = session.createQuery(
+		                "SELECT E.empNo, E.apellido, E.salario " +
+		                "FROM Empleados E " +
+		                "WHERE E.departamentos.dnombre = :dep5 " +
+		                "AND E.salario > 1500", Object[].class);
+		        query5.setParameter("dep5", dep5);
+		        List<Object[]> resultados5 = query5.list();
+		        resultados5.forEach(r -> System.out.println("Emp_no: " + r[0] + ", " + r[1] + ", " + r[2] + "EUR."));
 	
 	        } catch (Exception e) {
 	        	System.out.println("Exception en session: " + e);
