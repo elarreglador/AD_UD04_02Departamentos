9543b88d06ba058e199b4abd0b91e361362ca35f
elarreglador <elarreglador@protonmail.com>
2025-01-24 18:47:56 +0100

    ✨ Cambio menor: Agregada HQL query SELECT FROM WHERE

--- src/main/java/PruebaHibernate.java
+++ src/main/java/PruebaHibernate.java
@@ -3,6 +3,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
+
 import ud04_02Departamentos.entity.Empleados;
 import ud04_02Departamentos.entity.Departamentos;
 
@@ -10,30 +12,37 @@ import java.util.List;
 
 public class PruebaHibernate {
     public static void main(String[] args) {
-        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-        Session session = factory.openSession();
-        
-        System.out.println("Listado 1");
-        Query<Empleados> query = session.createQuery( "from Empleados", Empleados.class );
-        List<Empleados> list = query.list();
-        list.forEach(e-> System.out.println( e.toString() ) );
-        
-        System.out.println("Listado 2");
-        session.createQuery("from Empleados", Empleados.class)
-        	.list()
-        	.forEach(e-> System.out.println( e.toString() ) );
-        
-        System.out.println("Listado 3");
-        Query<Departamentos> query2 = session.createQuery( "from Departamentos", Departamentos.class );
-        List<Departamentos> list2 = query2.list();
-        list2.forEach(e-> System.out.println( e.toString() ) );
-        
-        System.out.println("Listado 4");
-        session.createQuery("from Departamentos", Departamentos.class)
-        	.list()
-        	.forEach(e-> System.out.println( e.toString() ) );
-
-        session.close();
-        factory.close();
+    	try ( SessionFactory sf = new Configuration().configure().buildSessionFactory() ){
+	        try ( Session session = sf.openSession() ) {
+		        
+		        // Muchas consultas requieren un @OverWrite de toString() del objeto
+		        // Consulta de empleados ( imprime objeto.toString() )
+		        System.out.println("Listado 1: Listado de Empleados");
+		        Query<Empleados> query = session.createQuery( "FROM Empleados", Empleados.class );
+		        List<Empleados> list = query.list();
+		        list.forEach(e-> System.out.println( e.toString() ) );
+		        
+		        // Consulta de empleados mas elegante ( imprime objeto.toString() )
+		        System.out.println("Listado 2: Listado de Empleados");
+		        session.createQuery("FROM Empleados", Empleados.class)
+		        	.list()
+		        	.forEach(e-> System.out.println( e.toString() ) );
+		        
+		        // Empleados que cobren mas de un minimo ( imprime dos campos )
+		        float minimo = 1000f;
+		        Query<Object[]> query3 = session.createQuery(
+		                "SELECT E.apellido, E.salario " +
+		                "FROM Empleados E " +
+		                "WHERE E.salario > :minimo", Object[].class);
+		        query3.setParameter("minimo", minimo);
+		        List<Object[]> resultados = query3.list();
+		        resultados.forEach(r -> System.out.println( r[0] + " | " + r[1]));
+	
+	        } catch (Exception e) {
+	        	System.out.println("Exception en session: " + e);
+	        }
+    	} catch (Exception e) {
+        	System.out.println("Exception en SessionFactory: " + e);
+    	}
     }
 }
